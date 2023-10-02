e38d5f75c2394489c6c771a7dc51bb92554a24de
ericdoppelt <ead45@duke.edu>
2023-10-02 00:04:46 -0400

    fixed covie policy setting

--- frontend/src/components/Onboarding/Integrations/Covie.js
+++ frontend/src/components/Onboarding/Integrations/Covie.js
@@ -3,13 +3,18 @@ import { Logger } from 'aws-amplify';
 
 const logger = new Logger('Plaid');
 
-export default function Covie({ setCoviePolicies, onSuccess, onClose }) {
+export default function Covie({ setCoviePolicies, onSuccess }) {
+
+  const onLinkSuccess = (linkId, policies) => {
+    setCoviePolicies(policies);
+    onSuccess();
+  };
 
   // Establish the link button. This code was provided by Covie.
   useEffect(() => {
     window.Covie.access.init({
       integrationKey: 'ik_tgvz5zp57bq5jrij',
-      onSuccess: onSuccess,
+      onSuccess: onLinkSuccess,
     });
   }, []);
 
