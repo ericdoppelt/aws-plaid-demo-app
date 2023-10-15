239858fdc9b3555bd1a3850c3f581c44be99b209
Dylan Wallach <22645038+FireWallach@users.noreply.github.com>
2023-10-14 20:02:15 -0400

    Modal Changes

--- frontend/src/components/Onboarding/Steps/OnboardingForm.js
+++ frontend/src/components/Onboarding/Steps/OnboardingForm.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Button from '@mui/material/Button';
+import { Checkbox } from '@mui/material';
+import { FormControl, FormLabel, RadioGroup, Radio } from '@mui/material'
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -27,13 +29,32 @@ export default function OnboardingForm({
 
   return (
     <Dialog open={true} onClose={onClose}>
-      <DialogTitle>Generate Claimant Form!</DialogTitle>
+      <DialogTitle><b>Record Retrieval Request</b></DialogTitle>
       <DialogContent>
-        <DialogContentText>Please note the information that you would like to pull for the claimant.</DialogContentText>
         <FormGroup>
-          <FormControlLabel control={<Switch checked={false} disabled />} label="Health Data" />
+          <DialogContentText>Demographic Information</DialogContentText>
+          <TextField
+            id="outlined-helperText"
+            label="Client First Name"
+            style={{ marginTop: '16px', width: '50%' }}
+          />
+          <TextField
+            id="outlined-helperText"
+            label="Client Last Name"
+            style={{ marginTop: '16px', width: '50%' }}
+          />
+          <TextField
+            id="outlined-helperText"
+            label="Client Email Address"
+            style={{ marginTop: '16px', marginBottom: '30px', width: '50%' }}
+          />
+          <DialogContentText>Document Selection</DialogContentText>
           <FormControlLabel
-            control={<Switch checked={plaidEnabled} onChange={(event) => setPlaidEnabled(event.target.checked)} />}
+            control={<Checkbox />}
+            label="Health Records"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={plaidEnabled} onChange={(event) => setPlaidEnabled(event.target.checked)} />}
             label="Employment Data"
           />
           {/* Only render the # input if the toggle is checked for Plaid. */}
@@ -51,15 +72,35 @@ export default function OnboardingForm({
             />
           ) : null}
           <FormControlLabel
-            control={<Switch checked={covieEnabled} onChange={(event) => setCovieEnabled(event.target.checked)} />}
-            label="Auto Insurance Data"
+            control={<Checkbox checked={covieEnabled} onChange={(event) => setCovieEnabled(event.target.checked)} />}
+            label="Auto Insurance Policy"
           />
+          <FormControlLabel
+            control={<Checkbox />}
+            style={{ marginBottom: '30px' }}
+            label="Health Insurnace Policy"
+          />
+          <FormControl style={{marginBottom:'40px'}}>
+            <FormLabel id="demo-radio-buttons-group-label">Sharing Preference</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="openForm"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel value="link" control={<Radio />} label="Share via link" />
+              <FormControlLabel value="openForm" control={<Radio />} label="Open form in browser" />
+            </RadioGroup>
+          </FormControl>
         </FormGroup>
+        <DialogActions>
+          <Button variant="outlined" style={{ marginLeft: '8px', marginRight: '160px', width: '21em' }} onClick={onClose}>
+            Back
+          </Button>
+          <Button variant="contained" onClick={onSuccess} style={{width: '21em'}}>
+            Request Records
+          </Button>
+        </DialogActions>
       </DialogContent>
-      <DialogActions>
-        <Button variant="outlined" color="error" onClick={onClose}>Cancel</Button>
-        <Button variant="contained" onClick={onSuccess}>Generate</Button>
-      </DialogActions>
     </Dialog>
   );
 }
